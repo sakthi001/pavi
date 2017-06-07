80a41b82a41da4eccaa7df86b5de1e8b0dc5bb36
sakthi001 <sakthivelpavi1997@gmail.com>
2017-06-07 14:54:30 +0530

    Add files via upload

---
+++ pavi.java
@@ -0,0 +1,24 @@
+package positive;
+import java.util.Scanner;
+public class positive {
+		public static void main(String args[])
+		{
+		int n1;
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the number");
+		no=in.nextInt();
+		if(n1==0)
+		{
+		System.out.println("The given no "+n1+" is zero");
+		}
+		if(n1>0)
+		{
+		System.out.println("The given no "+n1+" is positive");
+		}
+		if(n1<0)
+		{
+		System.out.println("The given no "+n1+" is negative");
+		}
+		}
+		}
+
